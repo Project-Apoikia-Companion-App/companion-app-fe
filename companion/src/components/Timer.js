0eb3f04a947ddb20c78e9665666703d1a7a35839
Samuel Lee <samuelxlee@gmail.com>
2021-07-14 15:47:19 -0700

    utilized component to render timer and countdown logic properly

--- companion/src/components/Timer.js
+++ companion/src/components/Timer.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import Countdown, { zeroPad } from "react-countdown";
 
 const Timer = ({ date, addTime }) => {
+    const countdownRef = useRef();
+
+    const start = () => {
+        countdownRef.current.start();
+    }
+
     const addFifteen = () => {
         addTime(15000);
+        start();
+        console.log(countdownRef.current.isCompleted());
     }
 
     const addThirty = () => {
         addTime(30000);
+        start();
     }
 
     const addSixty = () => {
         addTime(60000);
+        start();
     }
 
     const renderer = ({ minutes, seconds }) => {
@@ -28,16 +38,17 @@ const Timer = ({ date, addTime }) => {
                 date={date}
                 renderer={renderer}
                 controlled={false}
+                ref={countdownRef}
             />
+            <div style={{ textAlign: 'center' }}>
+                <button onClick={() => countdownRef.current.pause()}>Pause</button>
+            </div>
             <div style={{ textAlign: 'center' }}>
                 <button onClick={() => addFifteen()}>:15</button>
                 <button onClick={() => addThirty()}>:30</button>
                 <button onClick={() => addSixty()}>:60</button>
             </div>
-            <div style={{ textAlign: 'center' }}>
-                <button>Start</button>
-            </div>
-        </React.Fragment>
+        </React.Fragment >
     );
 };
 
